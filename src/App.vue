dacd34301c8806dcf1235e39793aa3e857884b4e
Vuth-Menghuor <hourousa9@gmail.com>
2025-05-23 07:26:57 +0700

    connect frontend to backend and implement button functionality

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         >You have <span class="pending-num"> {{ nbOfTodo }} </span> tasks
         pending.</span
       >
-      <button class="clear-button" @click="clearAll">Clear All</button>
+      <button class="clear-button" @click="clearAllTodos">Clear All</button>
     </div>
   </div>
 </template>
@@ -29,6 +29,7 @@ export default {
       store,
     };
   },
+
   components: {
     AddTodo,
     TodoLists,
